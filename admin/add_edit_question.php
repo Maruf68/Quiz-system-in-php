58b578f9ab65f8aea4a32a1a32bd7e5dea738b74
Maruf68 <maruf.akib73@gmail.com>
2022-06-01 11:01:00 +0600

    question and questions with pictures are now showing

--- admin/add_edit_question.php
+++ admin/add_edit_question.php
@@ -38,7 +38,7 @@
 
 
                         <div class="col-lg-6">
-                            <form name="from1" action="" method="post" enctype="multipart/form-data" >
+                            <form name="from1" action="" method="post" enctype="multipart/form-data">
                                 <div class="card">
                                     <div class="card-header"><strong>Add new questions with text</strong>
                                         <div class="card-body card-block">
@@ -82,57 +82,57 @@
                                     </div>
 
                                 </div>
-                            
+
 
                         </div>
 
 
 
                         <div class="col-lg-6">
-                        
-                                <div class="card">
-                                    <div class="card-header"><strong>Add new questions with images</strong>
-                                        <div class="card-body card-block">
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Question</label><input type="text" placeholder="Add Question"
-                                                    class="form-control"  name="fquestion"></div>
+                            <div class="card">
+                                <div class="card-header"><strong>Add new questions with images</strong>
+                                    <div class="card-body card-block">
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Option 1</label><input type="file" 
-                                                    class="form-control" style="padding-bottom:35px" name="fopt1"></div>
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Question</label><input type="text" placeholder="Add Question"
+                                                class="form-control" name="fquestion"></div>
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Option 2</label><input type="file"
-                                                    class="form-control" style="padding-bottom:35px" name="fopt2"></div>
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Option 1</label><input type="file" class="form-control"
+                                                style="padding-bottom:35px" name="fopt1"></div>
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Option 3</label><input type="file"
-                                                    class="form-control" style="padding-bottom:35px" name="fopt3"></div>
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Option 2</label><input type="file" class="form-control"
+                                                style="padding-bottom:35px" name="fopt2"></div>
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Option 4</label><input type="file"
-                                                    class="form-control" style="padding-bottom:35px" name="fopt4"></div>
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Option 3</label><input type="file" class="form-control"
+                                                style="padding-bottom:35px" name="fopt3"></div>
 
-                                            <div class="form-group"><label for="company" class=" form-control-label">Add
-                                                    Answer</label><input type="file"
-                                                    class="form-control" name="fanswer"></div>
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Option 4</label><input type="file" class="form-control"
+                                                style="padding-bottom:35px" name="fopt4"></div>
 
-
+                                        <div class="form-group"><label for="company" class=" form-control-label">Add
+                                                Answer</label><input type="file" class="form-control" name="fanswer">
+                                        </div>
 
 
 
 
-                                            <div class="form-group">
-                                                <input type="submit" name="submit2" value="Add question"
-                                                    class="btn btn-success">
 
 
-                                            </div>
+                                        <div class="form-group">
+                                            <input type="submit" name="submit2" value="Add question"
+                                                class="btn btn-success">
+
+
                                         </div>
                                     </div>
-
                                 </div>
+
+                            </div>
                             </form>
 
                         </div>
@@ -148,6 +148,109 @@
 
             </div>
             <!--/.col-->
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">                      
+                        <table class="table table-bordered">
+                            <tr>
+                                     <th>No</th>
+                                     <th>Questions</th>
+                                     <th>opt1</th>
+                                     <th>opt2</th>
+                                     <th>opt3</th>
+                                     <th>opt4</th>
+                                     <th>edit</th>
+                                     <th>delete</th>
+                                     
+
+                            </tr>
+
+                            <?php
+                         $sql=mysqli_query($link,"select * from `questions` where category='$exam_category' order by question_no asc");
+                           while($row=mysqli_fetch_array($sql))
+
+                           {
+                               echo "<tr>";
+                               echo "<td>"; echo $row["question_no"];  echo "</td>";
+                               echo "<td>"; echo $row["question"]; echo "</td>";
+                               echo "<td>"; 
+
+                               if(strpos($row["opt1"],'opt_images/')!==false)
+                               {
+                                   ?>
+                                  <img src="<?php echo $row["opt1"]; ?>" height="50" width="50"> 
+
+                                   <?php
+                               }
+                                else{
+                                    echo $row["opt1"];
+                                }
+
+
+                               echo "</td>";
+                               echo "<td>"; 
+
+                               if(strpos($row["opt2"],'opt_images/')!==false)
+                               {
+                                   ?>
+                                  <img src="<?php echo $row["opt2"]; ?>" height="50" width="50"> 
+
+                                   <?php
+                               }
+                                else{
+                                    echo $row["opt2"];
+                                }
+
+
+                               echo "</td>";
+                               echo "<td>"; 
+
+                               if(strpos($row["opt3"],'opt_images/')!==false)
+                               {
+                                   ?>
+                                  <img src="<?php echo $row["opt3"]; ?>" height="50" width="50"> 
+
+                                   <?php
+                               }
+                                else{
+                                    echo $row["opt3"];
+                                }
+
+
+                               echo "</td>";
+                               echo "<td>"; 
+
+                               if(strpos($row["opt4"],'opt_images/')!==false)
+                               {
+                                   ?>
+                                  <img src="<?php echo $row["opt4"]; ?>" height="50" width="50"> 
+
+                                   <?php
+                               }
+                                else{
+                                    echo $row["opt4"];
+                                }
+
+
+                               echo "</td>";
+
+                               echo "</tr>";
+                                      
+                           }
+
+
+                      ?> 
+
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
         </div>
 
 
@@ -182,11 +285,11 @@ else{
      $loop=$loop+1;
      mysqli_query($link,"INSERT INTO `questions`(`id`, `question_no`, `question`, `opt1`, `opt2`, `opt3`, `opt4`, `answer`, `category`) VALUES (NULL,'$loop','$_POST[question]','$_POST[opt1]','$_POST[opt2]','$_POST[opt3]','$_POST[opt4]','$_POST[answer]','$exam_category')") or die(mysqli_error($link));
      ?>
-        <script type="text/javascript">
-            alert("question added successfully");
-        </script>
+<script type="text/javascript">
+alert("question added successfully");
+</script>
 
-       <?php
+<?php
 
 }
 
@@ -223,47 +326,47 @@ else{
 
        $tm=md5(time());
        $fnm1=$_FILES["fopt1"]["name"];
-       $dst1="./opt_images/" . $tm . $fnm1;  //uploading image
-       $dst_db1="opt_images/" . $tm. $fnm1;  //downloading image
+       $dst1="./opt_images/".$fnm1;  //uploading image
+       $dst_db1="opt_images/".$fnm1;  //downloading image
        move_uploaded_file($_Files["fopt1"]["tmp_name"],$dst1);
 
 
 
        $tm=md5(time());
        $fnm2=$_FILES["fopt2"]["name"];
-       $dst2="./opt_images/" . $tm . $fnm2; 
-       $dst_db2="opt_images/". $tm . $fnm2;
+       $dst2="./opt_images/".$fnm2; 
+       $dst_db2="opt_images/".$fnm2;
        move_uploaded_file($_Files["fopt2"]["tmp_name"],$dst2);
 
 
        
        $tm=md5(time());
        $fnm3=$_FILES["fopt3"]["name"];
-       $dst3="./opt_images/" . $tm. $fnm3; 
-       $dst_db3="opt_images/". $tm . $fnm3;
+       $dst3="./opt_images/".$fnm3; 
+       $dst_db3="opt_images/".$fnm3;
        move_uploaded_file($_Files["fopt3"]["tmp_name"],$dst3);
 
        $tm=md5(time());
        $fnm4=$_FILES["fopt4"]["name"];
-       $dst4="./opt_images/". $tm . $fnm4; 
-       $dst_db4="opt_images/". $tm . $fnm4;
+       $dst4="./opt_images/".$fnm4; 
+       $dst_db4="opt_images/".$fnm4;
        move_uploaded_file($_Files["fopt4"]["tmp_name"],$dst4);
 
 
        $tm=md5(time());
        $fnm5=$_FILES["fanswer"]["name"];
-       $dst5="./opt_images/" . $tm. $fnm5; 
-       $dst_db5="opt_images/". $tm . $fnm5;
+       $dst5="./opt_images/".$fnm5; 
+       $dst_db5="opt_images/".$fnm5;
        move_uploaded_file($_Files["fanswer"]["tmp_name"],$dst5);
 
        mysqli_query($link,"INSERT INTO `questions`(`id`, `question_no`, `question`, `opt1`, `opt2`, `opt3`, `opt4`, `answer`, `category`) VALUES (NULL,'$loop','$_POST[fquestion]','$dst_db1','$dst_db2','$dst_db3','$dst_db4','$dst_db5','$exam_category')") or die(mysqli_error($link));
       
        ?>
-       <script type="text/javascript">
-           alert("question added successfully");
-       </script>
+<script type="text/javascript">
+alert("question added successfully");
+</script>
 
-      <?php
+<?php
 
 
 }
